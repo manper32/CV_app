6e1622e8c944a110bd05766fad0adec52cf362e2
manper32 <manuel_perez_32@outlook.com.com>
2020-07-24 12:31:39 -0500

    cv_app col claro

---
+++ cv_app_d/urls.py
@@ -0,0 +1,24 @@
+"""cv_app_d URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from cv_app.views import excel_CV_COL,csv_CV_Claro
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('excel_CV_Col/', excel_CV_COL),
+    path('csv_CV_Claro/', csv_CV_Claro),
+]
